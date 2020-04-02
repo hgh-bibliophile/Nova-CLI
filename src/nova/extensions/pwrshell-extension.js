43ba68c3a12d554eda8acbfae3859884d134083e
hgh.bibliophile <60857936+hgh-bibliophile@users.noreply.github.com>
2020-04-02 16:44:31 -0400

    Add Listr Functionality

--- src/nova/extensions/pwrshell-extension.js
+++ src/nova/extensions/pwrshell-extension.js
@@ -1,4 +1,4 @@
-module.exports = (nova) => {//TODO: Switch to execa & listr
+module.exports = (nova) => {//IDEA: Switch to execa?
     nova.ext.cmd = (cmd, args) => {
         const {
             PSCommand
@@ -8,9 +8,6 @@ module.exports = (nova) => {//TODO: Switch to execa & listr
     }
     nova.ext.run = (cmd, [spinner, message]) => {
         const shell = require('node-powershell');
-        const CLI = require('clui');
-        const Spinner = CLI.Spinner;
-        const log = require('log-utils');
         let ps = new shell({
             verbose: false,
             executionPolicy: 'Bypass',
@@ -23,21 +20,15 @@ module.exports = (nova) => {//TODO: Switch to execa & listr
         } else {
             ps.addCommand(cmd);
         }
-        const status = new Spinner(spinner || "Running");
-
         // Promise
         var promise = new Promise(function(resolve) {
-            status.start();
             ps.invoke()
                 .then(output => {
-                    status.stop();
-                    console.log(`${log.success} ${message}`);
                     resolve(true);
                     ps.dispose();
                 })
                 .catch(err => {
-                    status.stop()
-                    nova.log(err);
+                    console.log(err);
                     ps.dispose();
                 });
         })
--- src/nova/scss.js
+++ src/nova/scss.js
@@ -1,27 +1,40 @@
 //scss.js
 module.exports = (nova) => {
-    nova.ext.scssAll = async (options) => {
-      //require('./extensions/scss-extension.js')(nova)
-      //require('./extensions/dest-extension.js')(nova, options)
-      require('./extensions.js')(nova, options)
-      const color = require('log-utils')
-      const { dest } = nova.ext
-      const dir = await dest("styles")
-      const css = await dest('css')
+  nova.ext.scssAll = async (options) => {
+    require('./extensions.js')(nova, options)
+    const color = require('log-utils')
+    const Listr = require('listr');
+    const { dest, scss, prefix, cssmin } = nova.ext
+    const dir = await dest("styles")
+    const css = await dest('css')
 
-      const { scss, prefix, cssmin } = nova.ext
-      const ran = new Promise ( async (resolve) => {
-        try {
-            await scss(dir, options)
-            await prefix([css, dir], options)
-            await cssmin([css, dir], options)
-            console.log(color.green(`SCSS Compiling and Optimizing Complete`))
-            return resolve(true)
-          } catch {
-            console.log(color.red(`${color.error} Error Occurred: SCSS Compiling Didn't Finish`))
-            return resolve(true)
-          }
-      })
-      return ran
-    }
+    const tasks = new Listr([{
+        title: `Compile .scss files`,
+        task: () => scss(dir, options)
+      },
+      {
+        title: `Prefix .css files`,
+        task: () => prefix([css, dir], options)
+      },
+      {
+        title: 'Minify .css files',
+        enabled: () => options.pro,
+        task: () => cssmin([css, dir], options)
+      }
+    ])
+
+    const ran = new Promise(async (resolve) => {
+      try {
+        await tasks.run().catch(err => {
+          console.error(err)
+        })
+        console.log(color.green(`SCSS Compiling and Optimizing Complete`))
+        return resolve(true)
+      } catch {
+        console.log(color.red(`${color.error} Error Occurred: SCSS Compiling Didn't Finish`))
+        return resolve(true)
+      }
+    })
+    return ran
+  }
 }
