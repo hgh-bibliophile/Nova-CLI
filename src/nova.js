b36fae761d611951606eaa6f924861d5e11c54cb
hgh.bibliophile <60857936+hgh-bibliophile@users.noreply.github.com>
2020-04-05 17:58:19 -0400

    Added execution timing & began to add .js functions

--- src/nova.js
+++ src/nova.js
@@ -1,5 +1,7 @@
 module.exports = {
   run: async () => {
+    const perf = require('execution-time')() // Dev Test
+    perf.start()
     const { program } = require('commander')
     const project = require('../package.json')
     const nova = program
@@ -14,6 +16,18 @@ module.exports = {
         require('./nova/scss.js')(nova)
         const { scssAll } = nova.ext
         await scssAll(args)
+        const results = perf.stop()
+        console.log(results.preciseWords)
+      })
+    nova
+      .command('js')
+      .description('Transpile .js files')
+      .option('-d, --dev', 'Run in dev mode.', false)
+      .option('-p, --pro', 'Run in pro mode.', false)
+      .action(async args => {
+        require('./nova/js.js')(nova)
+        const { jsAll } = nova.ext
+        await jsAll(args)
       })
     nova.parse(process.argv)
   }
