316faf80b1a764fe7fa43c41b5a9d12f8537429a
hgh.bibliophile <60857936+hgh-bibliophile@users.noreply.github.com>
2020-04-08 22:50:23 -0400

    v1.0.0 .exe

--- src/nova/extensions/scss-extension.js
+++ src/nova/extensions/scss-extension.js
@@ -1,6 +1,7 @@
-const sass = 'sass'
-const postcss = 'postcss'
 const execa = require('execa')
+const path = require('path')
+const sass = path.join(__dirname, '../../../node_modules/sass/sass.js')
+const postcss = path.join(__dirname, '../../../node_modules/postcss-cli/bin/postcss')
 module.exports = (nova) => {
   nova.ext.scss = (outputDir, args) => {
     const dir = require('../../config/pathVar.js')
@@ -11,10 +12,8 @@ module.exports = (nova) => {
         ? `--no-source-map`
         : `--source-map`
       try {
-        await execa(
-          `${sass}`,
-          [`${srcmps}`, `${dir.src.styles}:${outputDir}`],
-          { preferLocal: true }
+        await execa.command(
+          `${sass} ${srcmps} ${dir.src.styles}:${outputDir}`
         )
         resolve(true)
       } catch (error) {
@@ -28,8 +27,7 @@ module.exports = (nova) => {
       const srcmps = args.dev ? `--map` : args.pro ? `--no-map` : `--map`
       try {
         await execa.command(
-          `${postcss} ${srcDir} ${srcmps} --use autoprefixer --dir ${outDir}`,
-          { preferLocal: true }
+          `${postcss} ${srcDir} ${srcmps} --use autoprefixer --dir ${outDir}`
         )
         resolve(true)
       } catch (error) {
@@ -44,8 +42,7 @@ module.exports = (nova) => {
       if (args.pro) {
         try {
         await execa.command(
-          `${postcss} ${srcDir} --no-map --use cssnano --dir ${outDir}`,
-          { preferLocal: true }
+          `${postcss} ${srcDir} --no-map --use cssnano --dir ${outDir}`
         ).then(nova.ext.ext(outDir, 'css', 'min'))
         resolve(true)
       } catch (error) {
