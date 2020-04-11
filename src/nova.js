d73132fd063bd5f3cb2e85d93958614a01c9172a
hgh.bibliophile <60857936+hgh-bibliophile@users.noreply.github.com>
2020-04-11 10:26:59 -0400

    More work on Prettify command

--- src/nova.js
+++ src/nova.js
@@ -1,10 +1,16 @@
+
+
 module.exports = {
   run: async () => {
     const perf = require('execution-time')() // Dev Test
     perf.start()
+    //Functions
     const logTime = () => {
       const results = perf.stop()
       console.log("Run Time: "+results.preciseWords)
+    }
+    const argVal = (opt) => {
+      opt = opt === undefined ? false : opt
     }
     const { program } = require('commander')
     const project = require('../package.json')
@@ -20,16 +26,16 @@ module.exports = {
       .command('scss')
       .alias('s')
       .description('Compile .scss files')
-      .option('-d, --dev', 'Run in dev mode.', false)
-      .option('-p, --pro', 'Run in pro mode.', false)
+      .option('-d, --dev', 'Run in dev mode.')
+      .option('-p, --pro', 'Run in pro mode.')
       .action(async args => {
         require('./nova/scss.js')(nova)
         const { scssAll } = nova.ext
         await scssAll(args)
         logTime()
-      })
+      })// Or 'prettier', 'pretty'
     nova
-      .command('prettify <dir> [file types]')// Or 'prettier', 'pretty'
+      .command('prettify <dir>')
       .alias('p')
       .description('Prettify files in directory')
       .option('-h, --html', 'Add .html files')
@@ -37,12 +43,17 @@ module.exports = {
       .option('-s, --scss', 'Add .scss files')
       .option('-j, --js', 'Add .js files')
       .option('-a, --all', 'Add all files')
-      .action(async (dir, options) => {
+      .action(async (dir, args) => {
         /*require('./nova/prettier.js')(nova)
         const { prettify } = nova.ext
         await prettify(args)*/
-        console.log(dir)
-        console.log(options.html)
+        args.html = args.html === undefined ? false : args.html
+        args.scss = args.scss === undefined ? false : args.scss
+        args.css = args.css === undefined ? false : args.css
+        args.js = args.js === undefined ? false : args.js
+        args.all = args.all === undefined ? false : args.all
+        console.log(args.opts())
+        console.log(args.html)
         logTime()
       })
     nova.parse(process.argv)
