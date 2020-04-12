4ac20715b777a2662b835d6de638e5154d01448c
hgh.bibliophile <60857936+hgh-bibliophile@users.noreply.github.com>
2020-04-12 15:14:02 -0400

    v2.0.0 .exe

--- src/nova/extensions/prettier-extension.js
+++ src/nova/extensions/prettier-extension.js
@@ -1,5 +1,6 @@
-const prettier = `prettier`
 const execa = require('execa')
+const path = require("path")
+const prettier = path.join(__dirname, "../../../node_modules/prettier/bin-prettier.js")
 module.exports = (nova) => {
     nova.ext.pretty = (outDir, dirc) => {
         const dir = require('../../config/pathVar.js')
@@ -9,9 +10,7 @@ module.exports = (nova) => {
         if (outDir === "all") fileDir = folderDir
         var prettypromise = new Promise(async (resolve, reject) => {
         try {
-          await execa.command(`${prettier} --use-tabs --write ${fileDir}`,
-            { preferLocal: true }
-          )
+          await execa.command(`${prettier} --use-tabs --write ${fileDir}`)
           resolve(true)
         } catch (error) {
           reject(error)
--- src/nova/extensions/scss-extension.js
+++ src/nova/extensions/scss-extension.js
@@ -1,58 +1,55 @@
-const sass = 'sass'
-const postcss = 'postcss'
-const execa = require('execa')
+const execa = require("execa")
+const path = require("path")
+const sass = path.join(__dirname, "../../../node_modules/sass/sass.js")
+const postcss = path.join(__dirname, "../../../node_modules/postcss-cli/bin/postcss")
 module.exports = (nova) => {
-  nova.ext.scss = (outputDir, args) => {
-    const dir = require('../../config/pathVar.js')
-    var scsspromise = new Promise(async (resolve, reject) => {
-      const srcmps = args.dev
-        ? `--source-map`
-        : args.pro
-        ? `--no-source-map`
-        : `--source-map`
-      try {
-        await execa(
-          `${sass}`,
-          [`${srcmps}`, `${dir.src.styles}:${outputDir}`],
-          { preferLocal: true }
-        )
-        resolve(true)
-      } catch (error) {
-        reject(error)
-      }
-    })
-    return scsspromise
-  },
-  nova.ext.prefix = ([srcDir, outDir], args) => {
-    var prefixpromise = new Promise(async (resolve, reject) => {
-      const srcmps = args.dev ? `--map` : args.pro ? `--no-map` : `--map`
-      try {
-        await execa.command(
-          `${postcss} ${srcDir} ${srcmps} --use autoprefixer --dir ${outDir}`,
-          { preferLocal: true }
-        )
-        resolve(true)
-      } catch (error) {
-        reject(error)
-      }
-    })
-    return prefixpromise
-  },
-  nova.ext.cssmin = ([srcDir, outDir], args) => {
-    require('./rename-extension')(nova)
-    var mincsspromise = new Promise(async (resolve, reject) => {
-      if (args.pro) {
-        try {
-        await execa.command(
-          `${postcss} ${srcDir} --no-map --use cssnano --dir ${outDir}`,
-          { preferLocal: true }
-        ).then(nova.ext.ext(outDir, 'css', 'min'))
-        resolve(true)
-      } catch (error) {
-        reject(error)
-      }
-    }
-    })
-    return mincsspromise
-  }
+	;(nova.ext.scss = (outputDir, args) => {
+		const dir = require("../../config/pathVar.js")
+		var scsspromise = new Promise(async (resolve, reject) => {
+			const srcmps = args.dev
+				? `--source-map`
+				: args.pro
+				? `--no-source-map`
+				: `--source-map`
+			try {
+				await execa.command(`${sass} ${srcmps} ${dir.src.styles}:${outputDir}`)
+				resolve(true)
+			} catch (error) {
+				reject(error)
+			}
+		})
+		return scsspromise
+	}),
+		(nova.ext.prefix = ([srcDir, outDir], args) => {
+			var prefixpromise = new Promise(async (resolve, reject) => {
+				const srcmps = args.dev ? `--map` : args.pro ? `--no-map` : `--map`
+				try {
+					await execa.command(
+						`${postcss} ${srcDir} ${srcmps} --use autoprefixer --dir ${outDir}`
+					)
+					resolve(true)
+				} catch (error) {
+					reject(error)
+				}
+			})
+			return prefixpromise
+		}),
+		(nova.ext.cssmin = ([srcDir, outDir], args) => {
+			require("./rename-extension")(nova)
+			var mincsspromise = new Promise(async (resolve, reject) => {
+				if (args.pro) {
+					try {
+						await execa
+							.command(
+								`${postcss} ${srcDir} --no-map --use cssnano --dir ${outDir}`
+							)
+							.then(nova.ext.ext(outDir, "css", "min"))
+						resolve(true)
+					} catch (error) {
+						reject(error)
+					}
+				}
+			})
+			return mincsspromise
+		})
 }
